cd1b962e6ab7ccd091cdb9182a15ad65a586264c
n-kumagai96 <n.kumagai.1996@gmail.com>
2026-08-30 10:04:25 +0900

    update GenerateProjectFile.bat

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -1,6 +1,8 @@
 @echo off
+cd /d "%~dp0.."
+
 echo Generating Visual Studio project...
 
-scons platform=windows target=editor vsproj=yes debug_symbols=yes dev_build=yes accesskit=no
+scons platform=windows target=editor vsproj=yes debug_symbols=yes dev_build=yes
 
 pause
